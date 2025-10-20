0db7c6681962ad6115b057b8b4146a2ff9fc5e9a
alaalshehabii <alaa.hsan.hajj@gmail.com>
2025-10-20 23:51:11 +0300

    completed react components lab

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,13 @@
+// src/components/WeatherForecast/WeatherForecast.jsx
+import './WeatherForecast.css';
+
+export default function WeatherForecast({ day, img, imgAlt, conditions, time }) {
+  return (
+    <div className="weather">
+      <h2>{day}</h2>
+      <img src={img} alt={imgAlt} />
+      <p><span>conditions: </span>{conditions}</p>
+      <p><span>time: </span>{time}</p>
+    </div>
+  );
+}
